221d4e929c03de08fb2962259ea05b11b92c2991
chltngus129 <chltngus129@gmail.com>
2021-12-27 11:34:47 +0900

    MyBatis 연동 및 XML Mapper

--- src/test/java/org/conan/test/DataSourceTest.java
+++ src/test/java/org/conan/test/DataSourceTest.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +21,29 @@ import lombok.extern.log4j.Log4j;
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class DataSourceTest {
+	// DataSource
 	@Setter(onMethod_={@Autowired})
 	private DataSource ds;
 	@Test
 	public void testConnection() {
 		try ( Connection con = ds.getConnection()) {
 			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	
+	
+	// MyBatis
+	@Setter(onMethod_= {@Autowired})
+	private SqlSessionFactory sqlSessionFactory;
+	
+	@Test
+	public void testMyBatis() {
+		try(SqlSession session = sqlSessionFactory.openSession();
+			Connection con = session.getConnection();) {
+			log.info(session); log.info(con);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
