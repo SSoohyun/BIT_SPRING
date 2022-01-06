ee8839042c84a511921124b7c395a69e79904353
chltngus129 <chltngus129@gmail.com>
2022-01-06 12:09:57 +0900

    업로드된 파일의 데이터 반환/썸네일 및 파일 화면에 보여주기/파일 다운로드(uuid 제거)

--- src/main/java/org/conan/controller/UploadController.java
+++ src/main/java/org/conan/controller/UploadController.java
@@ -3,15 +3,27 @@ package org.conan.controller;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+import org.conan.domain.AttachFileDTO;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.extern.log4j.Log4j;
@@ -72,9 +84,11 @@ public class UploadController {
 	}
 
 	// 업로드되는 파일 데이터 처리
-	@PostMapping("/uploadAjaxAction")
-	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+	@PostMapping(value= "/uploadAjaxAction", produces=MediaType.APPLICATION_JSON_VALUE) // json으로 받겠다
+	@ResponseBody // 응답
+	public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
 		String uploadFolder = "c:/upload";
+		List<AttachFileDTO> list = new ArrayList<>();
 
 		// 폴더 생성하기
 		File uploadPath = new File(uploadFolder, getFolder());
@@ -84,13 +98,16 @@ public class UploadController {
 		}
 
 		for (MultipartFile multipartFile : uploadFile) {
+			AttachFileDTO attachDTO = new AttachFileDTO();
+			
 			log.info("Upload File Name : " + multipartFile.getOriginalFilename());
 			log.info("Upload File Size : " + multipartFile.getSize());
 
 			// 중복된 이름의 파일 처리
 			UUID uuid = UUID.randomUUID();
-			String uploadFileName = multipartFile.getOriginalFilename();
-			uploadFileName = uuid.toString() + "_" + uploadFileName; // 뒤에 원래 파일 이름 붙이기
+			String uploadFileName = multipartFile.getOriginalFilename(); 
+			attachDTO.setFileName(uploadFileName); // 원래 파일 이름
+			uploadFileName = uuid.toString() + "_" + uploadFileName; // 파일 이름 설정: 뒤에 원래 파일 이름 붙이기
 
 			// 파일 저장
 			// 파일 저장 위치: uploadFolder -> uploadPath
@@ -98,18 +115,71 @@ public class UploadController {
 			File saveFile = new File(uploadPath, uploadFileName);
 			try {
 				multipartFile.transferTo(saveFile);
+				attachDTO.setUuid(uuid.toString()); // uuid
+				attachDTO.setUploadPath(getFolder()); // 파일 경로
 				
 				// 썸네일 생성 및 저장
 				if(checkImageType(saveFile)) {
+					attachDTO.setImage(true); // 이미지가 맞음
+					
 					// FileOutputStream: 주어진 File 객체가 가리키는 파일을 쓰기 위한 객체를 생성
 					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName)); // 썸네일은 s 붙여서 저장
 					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
 					thumbnail.close();
 				}
 				
+				list.add(attachDTO); // 리스트에 추가
+				log.info("attachDTO : " + attachDTO);
+				
 			} catch (Exception e) {
 				log.error(e.getMessage());
 			}
 		}
+		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+	
+	// 썸네일 이미지 보여주기
+	@GetMapping("/display")
+	@ResponseBody
+	public ResponseEntity<byte[]> getFile(String fileName) { // 파일 이름을 전달받아 헤더 이미지 데이터를 전송
+		log.info("fileName : " + fileName);
+		File file = new File("c:/upload/" + fileName);
+		log.info("file : " + file);
+		
+		ResponseEntity<byte[]> result = null;
+		try {
+			HttpHeaders header = new HttpHeaders();
+			header.add("Content-Type", Files.probeContentType(file.toPath())); // 적당한 마임 타입을 헤더에 추가
+			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	// 첨부파일 다운로드: 일반 파일은 클릭 -> 다운로드, 이미지는 클릭 -> 원본 파일 보여주기
+	@GetMapping(value="/download", produces=MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	@ResponseBody
+	public ResponseEntity<Resource> downloadFile(String fileName) {
+		log.info("download file : " + fileName);
+		Resource resource = new FileSystemResource("c:/upload/" + fileName);
+		log.info("resource : " + resource);
+		
+		String resourceName = resource.getFilename();
+		
+		// uuid 제거
+		String resourceOriginalName = resourceName.substring(resourceName.indexOf("_") + 1);
+		log.info("resourceOriginalName: " + resourceOriginalName);
+		HttpHeaders headers = new HttpHeaders();
+		
+		try {
+			// 다운로드 시 저장되는 이름 (uuid 제거한 것으로 변경함)
+			headers.add("Content-Disposition", "attachment; fileName=" + new String(resourceOriginalName.getBytes("UTF-8"), "ISO-8859-1"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
 	}
 }
--- src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -6,15 +6,46 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<style type="text/css">
+.uploadResult {
+	width: 100%;
+	background-color: #ddd;
+}
+
+.uploadResult ul {
+	display: flex;
+	flex-flow: row;
+	justify-content: center;
+	align-items: center;
+}
+
+.uploadResult ul li {
+	list-style: none;
+	padding: 10px;
+}
+
+.uploadResult ul li img {
+	width: 20px;
+}
+</style>
+
 </head>
 <body>
+	<h1>Upload w/i Ajax</h1>
 	<div class="uploadDiv">
 		<input type="file" name="uploadFile" multiple>
+	</div>
+	<div class="uploadResult">
+		<ul></ul>
 	</div>
 	<button id="uploadBtn">Upload</button>
 	
 	<script type="text/javascript">
 		$(document).ready(function() {
+			// 업로드 전에 <input type="file"> 객체가 포함된 <div> 복사
+			var cloneObj = $(".uploadDiv").clone(); 
+			
 			$("#uploadBtn").on("click", function(e) {
 				// ajax 이용하는 경우에는 FormData 객체 이용 (form 태그와 같은 역할)
 				var formData = new FormData();
@@ -37,8 +68,12 @@
 					contentType : false,
 					data : formData, // 전달할 데이터
 					type : 'POST',
+					dataType: 'json', // 데이터 타입: json
 					success : function (result) {
 						alert('Uploaded');
+						console.log(result);
+						showUploadedFile(result); // json 형태의 업로드 결과를 인자로 줌				
+						$(".uploadDiv").html(cloneObj.html()); // 업로드 성공 후 초기 상태로 복원
 					}
 				}); // $.ajax
 			});
@@ -57,6 +92,27 @@
 				}
 				return true;
 			}
+			
+			// 업로드된 결과는 JSON 형태로 수신했으므로
+			// 화면에 썸네일을 보여주는 등의 형태로 Ajax 처리 결과 출력
+			var uploadResult = $(".uploadResult ul");
+			function showUploadedFile(uploadResultArr) {
+				var str = "";
+				$(uploadResultArr).each(function(i, obj) {
+					if(!obj.image) { // 이미지가 아닌 경우
+						// 클릭 시 다운로드 가능하게 처리
+						var fileCallPath = encodeURIComponent(obj.uploadPath + "/" + obj.uuid + "_" + obj.fileName);
+						str += "<li><a href='/download?fileName=" + fileCallPath + "'><img src='/resources/images/attach.png'>" + obj.fileName + "</a></li>";
+					} else {
+						// str += "<li>" + obj.fileName + "</li>";
+						
+						// 썸네일 나오게 처리
+						var fileCallPath = encodeURIComponent(obj.uploadPath +  "/s_" + obj.uuid + "_" + obj.fileName);
+						str += "<li><img src='/display?fileName=" + fileCallPath + "'></li>";
+					}
+				});
+				uploadResult.append(str); // 요소 추가 (<li> 추가)
+			} // showUploadedFile
 		});
 	</script>
 </body>
